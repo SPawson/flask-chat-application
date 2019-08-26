1c471394d0fa8cf3b707ec0f348095ec35bb5ca1
Ubuntu <ubuntu@ip-172-31-90-231.ec2.internal>
2019-08-26 16:03:57 +0000

    added basic routing and chat functionality

--- run.py
+++ run.py
@@ -5,9 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello</h1>"
+    """Main page wih instructions"""
     
+    return "To send a message use /USERNAME/MESSAGE"
+
+@app.route('/<username>')
+def user(username):
+    return "Hi " + username
+    
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)   
     
 host = os.getenv('IP')
 port = int(os.getenv('PORT'))
+
 app.run(host, port, debug = True)
